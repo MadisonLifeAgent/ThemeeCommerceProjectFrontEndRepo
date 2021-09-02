b1479759e803c338202cfac4a0c9e033d17b1db1
Fue Moua <MadisonLifeAgent@gmail.com>
2021-09-02 14:40:21 -0500

    no changes, just trying to pull

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -12,8 +12,8 @@ class LoginForm extends Component {
     
     async makeLoginRequest(){
         try{
-            let formData = {username: this.state.user.username, password: this.state.user.password}
-            let response = await axios.post('https://localhost:44394/api/authentication/login', formData);
+            const jwt = localStorage.getItem('token');
+            let response = await axios.post('https://localhost:44394/api/authentication/login', { headers: {Authorization: 'Bearer ' + jwt}});
             console.log(response.data)
             this.setState({
                 user: response.data
