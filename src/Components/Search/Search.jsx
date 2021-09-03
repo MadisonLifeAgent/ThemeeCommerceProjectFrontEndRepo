8ea2b77ab8dda0b16aa15e8f240acdeb1eaefc2d
Fue Moua <MadisonLifeAgent@gmail.com>
2021-09-03 03:28:03 -0500

    modified search.jsx, but still not working

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -8,39 +8,41 @@ class Search extends Component {
     super(props);
     this.state = {
       user: props.user,
-      products: {},
+      products: [],
       category: '',
-      searchTerm: "",
+      searchTerm: '',
     }
   }
 
-  handleChange = (event) => {
-    console.log(event.target.value)
-    this.setState({
-        [event.target.name]: event.target.value,
-    });
-  }
-
-  handleSubmit = (event) => {
-      console.log(this.searchTerm);
-      event.preventDefault();
-      this.getSearchResults(this.state.products);
-  };
-
-  async getSearchResults(results){
+  async getSearchResults(searchInfo){
     try{
-      let response = await axios.get(`https://localhost:44394/api/product/searchresults/${this.searchTerm}`);
+      let searchTerm = searchInfo.searchTerm;
+
+      let response = await axios.get(`https://localhost:44394/api/product/searchresults/${searchTerm}`);
       // set results
       if (response){
           let results = response.data;
           console.log("good api call");
+          console.log(response.data);
           return results;
         }
     }
     catch(ex){
         console.log("bad call");
     }
-}
+  }
+
+  handleChange = (event) => {
+    this.setState({
+        [event.target.name]: event.target.value,
+    });
+  }
+
+  handleSubmit = (event) => {
+      event.preventDefault();
+      this.getSearchResults(this.state);
+  };
+
 
   render() {
     return(
@@ -50,13 +52,13 @@ class Search extends Component {
         {/* Displays search form */}
         <div>
             <form>
-              <input name="searchTerm" type="text" placeholder="Search Term" value={this.state.searchTerm} onChange={this.handleChange} />
+              <input name="searchTerm" type="text" placeholder="Search" value={this.state.searchTerm} onChange={this.handleChange} />
               <input type="submit" value="Submit" />
             </form>
         </div>
 
-        {/* Displays search form and results*/}
-        <SearchResults getSearchResults={this.getSearchResults} />
+        {/* Displays search form and results
+        <SearchResults getSearchResults={this.getSearchResults} /> */}
       </div>
     );
   }
