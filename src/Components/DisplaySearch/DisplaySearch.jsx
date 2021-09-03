dfc521be3ab9c54a3e6210d065c5f53353669522
Fue Moua <MadisonLifeAgent@gmail.com>
2021-09-03 17:49:43 -0500

    modified displaysearch results

--- src/Components/DisplaySearch/DisplaySearch.jsx
+++ src/Components/DisplaySearch/DisplaySearch.jsx
@@ -5,7 +5,7 @@ import { ListGroup, ListGroupItem } from 'react-bootstrap';
 const DisplaySearch = (props) => {
    const searchResults = props.results;
     
-   const showProdcuts = searchResults.products.map((item) => {
+   const showProducts = searchResults.products.map((item) => {
        const product = {
            productName: item.productName,
            productPrice: item.productPrice,
@@ -13,47 +13,40 @@ const DisplaySearch = (props) => {
        };
 
        // render the page based on searchterm and results
-       if (searchResults){
-           return (
-                <div>
-                    <h3>Search Results for "{searchResults.searchTerm}"</h3>
+        return (
+            <div>
+                {/* Show product details */}
+                <ListGroupItem>
+                    <p>{product.productName}, ${product.productPrice}</p>
 
                     {/* Show product details */}
-                    <ListGroupItem>
-                        <p>{product.productName}, ${product.productPrice}</p>
-
-                        {/* Show product details */}
-                        <div class="accordion" id="accordionBg">
-                            <div class="accordion-item">
-                                <h4 class="accordion-header" id="viewMore">
-                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">View More Product Details</button>
-                                </h4>
-                                <div id="collapseOne" class="accordion-collapse collapse close" aria-labelledby="headingOne" data-bs-parent="#accordionBg">
-                                    <p>Product Category: {product.CategoryName}</p>
-                                    <p>Product Description:  {product.productDescription}</p>
-                                    <p>Reviews and Ratings:</p>
-                                    <dl>
-                                        <dt></dt>
-                                            <dd></dd>
-                                    </dl>
-                                </div>
+                    <div class="accordion" id="accordionBg">
+                        <div class="accordion-item">
+                            <h4 class="accordion-header" id="viewMore">
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">View More Product Details</button>
+                            </h4>
+                            <div id="collapseOne" class="accordion-collapse collapse close" aria-labelledby="headingOne" data-bs-parent="#accordionBg">
+                                <p>Product Category: {product.CategoryName}</p>
+                                <p>Product Description:  {product.productDescription}</p>
+                                <p>Reviews and Ratings:</p>
+                                <dl>
+                                    <dt></dt>
+                                        <dd></dd>
+                                </dl>
                             </div>
                         </div>
-                    </ListGroupItem>
-                </div>
-           )
-       } else {
-            return (
-                <h3>No Results for "{searchResults.searchTerm}"</h3>
+                    </div>
+                </ListGroupItem>
+            </div>
         )
-       }
 
-   })
+    });
     
     return ( 
         <div>
+            <h3>Search Results for "{searchResults.searchTerm}"</h3>
             <ListGroup>
-                {showProdcuts}
+                {showProducts}
             </ListGroup>
         </div>
      );
